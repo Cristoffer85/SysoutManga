ab91482f3c480574c93ca9ea52ce668bbf86f437
Cristoffer <119351534+Cristoffer85@users.noreply.github.com>
2023-03-15 11:52:34 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hejhej Hopp och lek med C!");
+
+        System.out.println("Hejhej Hopp och lek med C!");
+        System.out.println("Hejhej Hopp och lek med C!");
+        System.out.println("Hejhej Hopp och lek med C!");
+        System.out.println("Hejhej Hopp och lek med C!");
+    }
+}
